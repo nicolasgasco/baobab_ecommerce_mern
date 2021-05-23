b47c90a4685ce9e8b421419717088a5bd8b2bae1
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-23 23:21:07 +0200

    Added import for departments

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ const app = express();
 // External routes from routes folder
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const departmentsRouter = require("./routes/departments");
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "build")));
@@ -25,11 +26,12 @@ if (app.get("env") === "development") {
 
 // Used to redirect to react page when a route other than index is refreshed by user
 app.use("/api/users", usersRouter);
+app.use("/api/departments", departmentsRouter);
 app.use("/", indexRouter);
 
-
 // Database connection
-mongoose.connect(
+mongoose
+  .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@sandbox.1ybr6.mongodb.net/bootcamp_final_project?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
   )
@@ -43,12 +45,6 @@ mongoose.connect(
     console.log(`Connected to database...`);
   })
   .catch((err) => console.error("Could not connect to MongoDB...", err));
-
-
-// Use Joi for input validation
-// First step is describing schema inside app.get or whatever https://codewithmosh.com/courses/293204/lectures/4516859
-// const schema = { name: Joi.string().min(3).required()}
-// const result = Joi.validate(req.body, schema)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
