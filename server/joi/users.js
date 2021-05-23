e38f0ae4a40910a21d697e75c2027091f3d08404
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-23 11:14:52 +0200

    Improved Joi validation

--- server/joi/users.js
+++ server/joi/users.js
@@ -5,11 +5,23 @@ const userSchemaJoi = Joi.object({
   name: Joi.string().min(2).max(25).required().trim(),
   surname: Joi.string().min(2).max(25).required().trim(),
   password: passwordComplexity().required().trim(),
-  email: Joi.string().email().required(),
+  email: Joi.when("modificationDate", {
+    is: null,
+    then: Joi.optional(),
+    otherwise: Joi.string().email(),
+  }),
   gender: Joi.string().valid("f", "m", "o").required().trim(),
   birthday: Joi.date()
-    .max(`${new Date().getMonth() + 1}-${new Date().getDate()}-${new Date().getFullYear() - 18}`)
-    .min(`${new Date().getMonth() + 1}-${new Date().getDate()}-${new Date().getFullYear() - 125}`)
+    .max(
+      `${new Date().getMonth() + 1}-${new Date().getDate()}-${
+        new Date().getFullYear() - 18
+      }`
+    )
+    .min(
+      `${new Date().getMonth() + 1}-${new Date().getDate()}-${
+        new Date().getFullYear() - 125
+      }`
+    )
     .required(),
   creationDate: Joi.date(),
   modificationDate: Joi.date(),
@@ -22,7 +34,7 @@ const userSchemaJoi = Joi.object({
     province: Joi.string().trim().min(2).max(25),
     city: Joi.string().trim().min(2).max(25),
     zip: Joi.string().trim().length(5),
-    street: Joi.string().trim().min(2).max(25),
+    street: Joi.string().trim().min(1).max(25),
     streetNumber: Joi.string().trim().min(2).max(25),
     doorNumber: Joi.string().trim().min(2).max(10),
     other: Joi.string().trim().min(2).max(50),
