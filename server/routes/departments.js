e1711724bb1e5adbd12ace5c3a472e0f9bd53134
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-24 14:41:55 +0200

    Improved departments GET route

--- server/routes/departments.js
+++ server/routes/departments.js
@@ -3,16 +3,23 @@ const router = express.Router();
 
 const mongoose = require("mongoose");
 
-const departmentSchema = require("../mongoose/departments");
+const departmentSchema = require("../models/departments");
 const Department = mongoose.model("Department", departmentSchema);
 
 // GET all departments of a specific language
 router.get("/", async (req, res) => {
   try {
     let language = req.query.lang;
+    let filter;
+    if (language) {
+      filter = { _id: 1 };
+      filter[`translations.${language}`] = 1;
+    }
 
     // Dictionary request
-    const users = await Department.find().sort(`translations.${language}`);
+    const users = await Department.find()
+      .sort(`translations.${language}`)
+      .select(filter);
     res.send({
       resultsFound: users.length,
       sortBy: language,
@@ -21,6 +28,10 @@ router.get("/", async (req, res) => {
   } catch (err) {
     console.log("Error: " + err.message);
     res.status(400).send({ error: err.message });
+  }
+
+  if (!res) {
+    res.send("Error");
   }
 });
 
