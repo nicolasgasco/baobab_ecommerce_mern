1fc6487496a9bb3fe7fb2af1b7c12e414d9b9caf
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-24 22:39:16 +0200

    Added Mongoose and Joi validation

---
+++ server/joi/products.js
@@ -0,0 +1,44 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+const productSchemaJoi = Joi.object({
+  completeName: {
+    brand: Joi.string().uppercase().min(2).max(30).trim(),
+    shortDesc: Joi.string().lowercase().min(2).min(25).trim(),
+    productName: Joi.string().min(2).max(25).trim(),
+    productDesc1: Joi.string().lowercase().min(2).max(50).trim(),
+    productDesc2: Joi.string().lowercase().min(2).max(50).trim(),
+    productDesc3: Joi.string().lowercase().min(2).max(50).trim(),
+    productGender: Joi.string()
+      .lowercase()
+      .valid("unisex", "male", "female")
+      .trim(),
+  },
+  stock: Joi.number().min(0).max(9999),
+  seller: Joi.string().min(2).max(30).trim(),
+  department: Joi.object({
+    id: Joi.objectId(),
+    name: Joi.string().max(100),
+    date: Joi.date(),
+  }),
+  pricingInfo: {
+    price: Joi.number().min(0.01).max(2000).precision(2),
+    priceHistory: Joi.array().items(
+      Joi.number().min(0.01).max(2000).precision(2)
+    ),
+  },
+  ecoInfo: {
+    originCountryCode: Joi.string()
+      .uppercase()
+      .trim()
+      .pattern(/^[A-Z]{3}$/),
+    productionCountryCode: Joi.string()
+      .uppercase()
+      .trim()
+      .pattern(/^[A-Z]{3}$/),
+    socialMission: Joi.string().trim().min(2).max(500),
+    environmentMission: Joi.string().trim().min(2).max(500),
+  },
+});
+
+module.exports = productSchemaJoi;
--- server/models/products.js
+++ server/models/products.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 const fetch = require("node-fetch");
 
+// Needed for custom validation later
+const departmentSchema = require("./departments");
+const Department = mongoose.model("Department", departmentSchema);
+
 const productSchema = new mongoose.Schema(
   {
-    nameComplete: {
+    completeName: {
       brand: {
         type: String,
         uppercase: true,
         trim: true,
         minLength: [2, "Brand too short"],
         maxLength: [30, "Brand too long"],
-        required: [true, "Brand is required"],
+        // required: [true, "Brand is required"],
       },
       shortDesc: {
         type: String,
@@ -18,20 +22,22 @@ const productSchema = new mongoose.Schema(
         lowercase: true,
         minLength: [2, "Short desc too short"],
         maxLength: [25, "Short desc too long"],
-        required: [true, "Short desc is required"]
+        // required: [true, "Short desc is required"],
       },
       productName: {
         type: String,
         trim: true,
         minLength: [2, "Name too short"],
         maxLength: [25, "Name too long"],
-        required: [true, "Product name is required"],
+        // required: [true, "Product name is required"],
         // Capitalizing every word
         set: (value) =>
-        value.split(" ").map((word) => {
-          return word.charAt(0).toUpperCase() + word.substr(1);
-
-        }).join(" "),
+          value
+            .split(" ")
+            .map((word) => {
+              return word.charAt(0).toUpperCase() + word.substr(1);
+            })
+            .join(" "),
       },
       productDesc1: {
         type: String,
@@ -39,7 +45,7 @@ const productSchema = new mongoose.Schema(
         trim: true,
         minLength: [2, "Description too short"],
         maxLength: [50, "Description too long"],
-        required: [true, "Product description is required"],
+        // required: [true, "Product description is required"],
       },
       productDesc2: {
         type: String,
@@ -62,11 +68,40 @@ const productSchema = new mongoose.Schema(
         enum: ["unisex", "male", "female"],
       },
     },
+    stock: {
+      type: Number,
+      min: [0, "You cannot have negative stock"],
+      max: [9999, "Current max stock is 9999"],
+      set: (value) => Math.round(value),
+    },
     seller: {
       type: String,
       trim: true,
       minLength: [2, "Description too short"],
       maxLength: [30, "Description too long"],
+    },
+    department: {
+      type: mongoose.Schema.Types.ObjectId,
+      trim: true,
+      lowercase: true,
+      // required: [true, "Department is required"],
+      ref: "Department",
+      validate: {
+        // Calling API to see if country code exists
+        validator: async function (value) {
+          try {
+            const department = await Department.findById(value);
+            if (!department) {
+              return false;
+            }
+            return true;
+          } catch (err) {
+            console.log("Error: " + err.message);
+            return false;
+          }
+        },
+        message: "Insert a valid ID",
+      },
     },
     pricingInfo: {
       price: {
@@ -74,7 +109,7 @@ const productSchema = new mongoose.Schema(
         min: [0.01, "Price cannot be smaller than 0.01"],
         max: [2000, "Current max price is 2000€"],
         set: (value) => value.toFixed(2),
-        required: [true, "Price is required"],
+        // required: [true, "Price is required"],
       },
       priceHistory: [
         {
@@ -82,69 +117,69 @@ const productSchema = new mongoose.Schema(
           min: [0.01, "Price history  cannot be smaller than 0.01"],
           max: [2000, "Current max price history is 2000€"],
           set: (value) => value.toFixed(2),
-          required: [true, "Price history is required"],
+          // required: [true, "Price history is required"],
         },
       ],
     },
-    // ecoInfo: {
-    //   originCountryCode: {
-    //     type: String,
-    //     trim: true,
-    //     uppercase: true,
-    //     match: [/^[A-Z]{3}$/, "Origin country code is not valid"],
-    //     validate: {
-    //       // Calling API to see if country code exists
-    //       validator: async function (value) {
-    //         try {
-    //           const res = await fetch(
-    //             `https://restcountries.eu/rest/v2/alpha/${value.toUpperCase()}`
-    //           );
-    //           const result = await res.json();
-    //           return result.status === undefined;
-    //         } catch {
-    //           const err = new Error("The service is currently not available");
-    //           console.log(err.message);
-    //         }
-    //       },
-    //       message: "Insert a valid country",
-    //     },
-    //   },
-    //   productionCountryCode: {
-    //     type: String,
-    //     trim: true,
-    //     uppercase: true,
-    //     required: true,
-    //     match: [/^[A-Z]{3}$/, "Production country code is not valid"],
-    //     validate: {
-    //       // Calling API to see if country code exists
-    //       validator: async function (value) {
-    //         try {
-    //           const res = await fetch(
-    //             `https://restcountries.eu/rest/v2/alpha/${value.toUpperCase()}`
-    //           );
-    //           const result = await res.json();
-    //           return result.status === undefined;
-    //         } catch {
-    //           const err = new Error("The service is currently not available");
-    //           console.log(err.message);
-    //         }
-    //       },
-    //       message: "Insert a valid country",
-    //     },
-    //   },
-    //   socialMission: {
-    //     type: String,
-    //     trim: true,
-    //     minLength: [2, "Social mission too short"],
-    //     maxLength: [50, "Social mission too long"],
-    //   },
-    //   environmentMission: {
-    //     type: String,
-    //     trim: true,
-    //     minLength: [2, "Environment mission too short"],
-    //     maxLength: [50, "Environment mission too long"],
-    //   },
-    // },
+    ecoInfo: {
+      originCountryCode: {
+        type: String,
+        trim: true,
+        uppercase: true,
+        match: [/^[A-Z]{3}$/, "Origin country code is not valid"],
+        validate: {
+          // Calling API to see if country code exists
+          validator: async function (value) {
+            try {
+              const res = await fetch(
+                `https://restcountries.eu/rest/v2/alpha/${value.toUpperCase()}`
+              );
+              const result = await res.json();
+              return result.status === undefined;
+            } catch {
+              const err = new Error("The service is currently not available");
+              console.log(err.message);
+            }
+          },
+          message: "Insert a valid country",
+        },
+      },
+      productionCountryCode: {
+        type: String,
+        trim: true,
+        uppercase: true,
+        // required: true,
+        match: [/^[A-Z]{3}$/, "Production country code is not valid"],
+        validate: {
+          // Calling API to see if country code exists
+          validator: async function (value) {
+            try {
+              const res = await fetch(
+                `https://restcountries.eu/rest/v2/alpha/${value.toUpperCase()}`
+              );
+              const result = await res.json();
+              return result.status === undefined;
+            } catch {
+              const err = new Error("The service is currently not available");
+              console.log(err.message);
+            }
+          },
+          message: "Insert a valid country",
+        },
+      },
+      socialMission: {
+        type: String,
+        trim: true,
+        minLength: [2, "Social mission too short"],
+        maxLength: [500, "Social mission too long"],
+      },
+      environmentMission: {
+        type: String,
+        trim: true,
+        minLength: [2, "Environment mission too short"],
+        maxLength: [500, "Environment mission too long"],
+      },
+    },
   },
   { collection: "products" }
 );
