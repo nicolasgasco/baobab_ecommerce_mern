d477c45393acad8e766708105bcb1294db931113
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-24 22:37:35 +0200

    Added routes for products

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ const app = express();
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const departmentsRouter = require("./routes/departments");
+const productsRouter = require("./routes/products");
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "build")));
@@ -27,6 +28,7 @@ if (app.get("env") === "development") {
 // Used to redirect to react page when a route other than index is refreshed by user
 app.use("/api/users", usersRouter);
 app.use("/api/departments", departmentsRouter);
+app.use("/api/products", productsRouter);
 app.use("/", indexRouter);
 
 // Database connection
@@ -45,6 +47,40 @@ mongoose
     console.log(`Connected to database...`);
   })
   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const productSchema = require("./models/products");
+const { ObjectId } = require("bson");
+const Product = mongoose.model("Product", productSchema);
+
+try {
+  const product = new Product({
+    completeName: {
+      brand: "Parafina   ",
+      shortDesc: "Gafas de sol",
+      productName: "aviator bench social Club",
+      productDesc1: "En material 100% riciclado",
+      productDesc2: "Lentes polarizadas",
+      productGender: "Unisex",
+      seller: "Parafina Co",
+    },
+    department: new ObjectId("60aa4fabd82af1469cdbda95"),
+    pricingInfo: {
+      price: 0.0234,
+      priceHistory: [10.01, 9, 3],
+    },
+    ecoInfo: {
+      originCountryCode: "ITA",
+      productionCountryCode: "ESP",
+      socialMission: "Bla bla social",
+      environmentMission: "Bla bla environment",
+    }
+  });
+  console.log(product.productName)
+  product.save();
+  console.log("Save on database")
+} catch (err) {
+  console.log(err.message);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ server/routes/products.js
@@ -0,0 +1,135 @@
+const express = require("express");
+const router = express.Router();
+
+// Data validation with joi
+const Joi = require("joi");
+// const userSchemaJoi = require("../joi/users");
+
+const mongoose = require("mongoose");
+
+// Initializing users
+const productSchema = require("../models/products");
+const Product = mongoose.model("Product", productSchema);
+
+// GET all products
+router.get("/", async (req, res) => {
+  try {
+    // Populating department name from another table
+    const products = await Product.find().populate("department", "name -_id");
+    res.send({
+      resultsFound: products.length,
+      results: products,
+    });
+  } catch (err) {
+    console.log("Error: " + err.message);
+    res.status(400).send({ error: err.message });
+  }
+});
+
+// // POST a new user
+// router.post("/", async (req, res) => {
+//   // First round of validation with Joi
+//   const joiValidation = userSchemaJoi.validate(req.body);
+//   if (joiValidation.error) {
+//     res.status(400).send({
+//       error: `${
+//         joiValidation.error.name
+//       } (Joi): ${joiValidation.error.details.map((err) => {
+//         return err.message;
+//       })}`,
+//     });
+//     return;
+//   }
+//   console.log("Joi validation successful");
+
+//   // Encrypt password now and not before, otherwise it cannot be validated
+//   req.body.password = encryptPassword(req.body.password);
+
+//   // Creating new mongoose user with body
+//   const user = new User(req.body);
+//   try {
+//     // Second round of validation with Mongoose
+//     await user.validate();
+//     console.log("Mongoose validation successful");
+
+//     // Saving in DB and sending result
+//     const result = await user.save();
+//     res.send({insertedCount: 1, result: result});
+//   } catch (err) {
+//     const errMessages = [];
+//     for (field in err.errors) {
+//       console.log(`Error: ${err.errors[field].message}`);
+//       errMessages.push(err.errors[field].message);
+//     }
+
+//     res.status(400).send({
+//       error: errMessages,
+//     });
+//   }
+// });
+
+
+// // PUT a specific user (with ID)
+// router.put("/:id", async (req, res) => {
+//   // Add a modification date
+//   req.body.modificationDate = new Date();
+
+//   // First round of validation with Joi
+//   const joiValidation = userSchemaJoi.validate(req.body);
+//   if (joiValidation.error) {
+//     res.status(400).send({
+//       error: `${
+//         joiValidation.error.name
+//       } (Joi): ${joiValidation.error.details.map((err) => {
+//         return err.message;
+//       })}`,
+//     });
+//     return;
+//   }
+//   console.log("Joi validation successful");
+
+//   const updatedUser = new User(req.body);
+//   try {
+//     // Validation with Mongoose
+//     await updatedUser.validate();
+
+//     updatedUser.password = encryptPassword(req.body.password);
+//     // Finding and updating at the same time
+//     const result = await User.findByIdAndUpdate(req.params.id, req.body, {
+//       new: true,
+//     });
+//     if (!result) {
+//       throw err;
+//     }
+//     res.send({ updatedCount: 1, updatedObj: result });
+//   } catch (err) {
+//     const errMessages = [];
+//     for (field in err.errors) {
+//       console.log(`Error: ${err.errors[field].message}`);
+//       errMessages.push(err.errors[field].message);
+//     }
+
+//     res.status(400).send({
+//       error: errMessages,
+//     });
+//   }
+// });
+
+// // DELETE a specific user (with ID)
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const result = await User.remove({ _id: req.params.id });
+//     if (result.deletedCount === 0) {
+//       throw new Error("User not found");
+//     }
+//     res.send({ deletedCount: 1, result: result });
+//   } catch (err) {
+//     console.log("Error: ", err.message);
+//     res.status(400).send({
+//       deletedCount: 0,
+//       error: err.message,
+//     });
+//   }
+// });
+
+module.exports = router;
