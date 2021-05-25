0b813b63e6e19a6ce9eddd35c53c4bfd4f2c6a50
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-25 22:09:18 +0200

    Added unique flag for Mongoose

--- server/models/departments.js
+++ server/models/departments.js
@@ -8,6 +8,7 @@ const departmentSchema = new mongoose.Schema(
         maxLength: [25, "Translation too long"],
         lowercase: true,
         trim: true,
+        unique: true,
         required: [true, "Translation is required"],
         },
         translations: {
@@ -30,51 +31,5 @@ const departmentSchema = new mongoose.Schema(
     },
   { collection: "departments" }
 );
-
-// const Department = mongoose.model("Department", departmentSchema);
-
-// arr = [
-//   {
-//     name: "automotive",
-//     translations: { es_es: "Coche y Moto", en_us: "Automotive" },
-//   },
-//   {
-//     name: "baby",
-//     translations: { es_es: "Bebé", en_us: "Baby" },
-//   },
-//   {
-//     name: "beauty",
-//     translations: { es_es: "Belleza", en_us: "Beauty" },
-//   },
-//   {
-//     name: "sports",
-//     translations: { es_es: "Deportes", en_us: "Sports" },
-//   },
-//   {
-//     name: "electronics",
-//     translations: { es_es: "Electrónica", en_us: "Electronics" },
-//   },
-//   {
-//     name: "luggage",
-//     translations: { es_es: "Equipaje", en_us: "Luggage" },
-//   },
-//   {
-//     name: "toys",
-//     translations: { es_es: "Juguetes", en_us: "Toys" },
-//   },
-//   {
-//     name: "fashion",
-//     translations: { es_es: "Ropa", en_us: "Fashion" },
-//   },
-//   {
-//     name: "shoes",
-//     translations: { es_es: "Zapatos", en_us: "Shoes" },
-//   },
-// ];
-
-// Department.insertMany(arr, function (err, docs) {
-//   console.log(docs);
-// });
-
 
 module.exports = departmentSchema;
