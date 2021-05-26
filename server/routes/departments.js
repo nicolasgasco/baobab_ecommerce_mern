d8045b39b5d3ea392abbf3cee45a1d56a94c7b25
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-26 23:10:18 +0200

    Removed try catch

--- server/routes/departments.js
+++ server/routes/departments.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const mongoose = require("mongoose");
 
+// const asyncMiddleware = require("../middleware/async");
+
+
 const departmentSchema = require("../models/departments");
 const Department = mongoose.model("Department", departmentSchema);
 
@@ -21,20 +24,15 @@ router.get("/", async (req, res) => {
     filter[`translations.${language}`] = 1;
   }
 
-  try {
-    // Database request
-    const departments = await Department.find()
-      .sort(`translations.${language}`)
-      .select(filter);
-    res.send({
-      resultsFound: departments.length,
-      sortBy: language,
-      results: departments,
-    });
-  } catch (err) {
-    console.log("Error: " + err.message);
-    res.status(400).send({ error: err.message });
-  }
+  // Database request
+  const departments = await Department.find()
+    .sort(`translations.${language}`)
+    .select(filter);
+  res.send({
+    resultsFound: departments.length,
+    sortBy: language,
+    results: departments,
+  });
 });
 
 // GET a specific department (used for validation too)
@@ -45,17 +43,12 @@ router.get("/:id", async (req, res) => {
     res.status(500).send("Error");
   }
 
-  try {
-    // Database request
-    const department = await Department.find({ _id: id });
-    res.send({
-      resultsFound: 1,
-      result: department[0],
-    });
-  } catch (err) {
-    console.log("Error: " + err.message);
-    res.status(400).send({ error: err.message });
-  }
+  // Database request
+  const department = await Department.find({ _id: id });
+  res.send({
+    resultsFound: 1,
+    result: department[0],
+  });
 });
 
 module.exports = router;
