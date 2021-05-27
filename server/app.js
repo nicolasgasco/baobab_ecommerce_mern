319b5ff7d86a238342671d3991e5c443d7df39a2
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-27 19:32:18 +0200

    Reorganized code

--- server/app.js
+++ server/app.js
@@ -9,16 +9,19 @@ const Joi = require("joi");
 
 // Calling variuos app.use in separate file
 const app = express();
+
+// Handle all err messages instead of using try/catch blocks
+require("express-async-errors");
 // Includes unhandled rejections and uncaught exceptions
 require("./startup/logging")();
 // Includes dotenv and Morgan
 require("./startup/config")(app);
 // Include connection to database
 require("./startup/db")(mongoose);
+// ObjectId for Joi
+require("./startup/validation")(Joi);
 // Includes helmet, path, and express async errors
 require("./startup/routes")(app);
-// ObjectId ofr Joi
-require("./startup/validation")(Joi);
 // Complete passport auth logic
 require("./startup/auth")(app, passport, session, mongoose);
 
