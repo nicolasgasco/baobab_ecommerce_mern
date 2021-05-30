930a8d6e90e3592546ba505cd6b1ef4a08515717
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-30 22:05:46 +0200

    Moved routes logic to external controllers

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,122 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-// Data validation with joi
-const Joi = require("joi");
-const productSchemaJoi = require("../joi/products");
-const asyncMiddleware = require("../middleware/async");
+const productsController = require("../controllers/products");
 
-const validateObjectId = require("../middleware/validateObjectId");
-
-// Initializing products
-const { productSchema, Product } = require("../models/products");
+const validateUuid = require("../middleware/validateUuid");
 
 // GET all products
-router.get("/", async (req, res) => {
-  if (!res) {
-    res.status(500).send("Error");
-  }
+router.get("/", productsController.getAllProducts);
 
-  const pageNumber = req.query.pageNum;
-  const pageSize = req.query.pageSize;
-
-  // Populating department name from another table, with name and without id
-  const products = await Product.find()
-    .populate("department", "name -_id")
-    .skip((pageNumber - 1) * pageSize)
-    .limit(+pageSize);
-
-  if (products.length === 0) {
-    res.status(400).send({ error: "No results found" });
-  }
-
-  res.send({
-    resultsFound: products.length,
-    pageNumber,
-    pageSize,
-    results: products,
-  });
-});
+//  GET a specific product with id
+router.get("/:id", validateUuid, productsController.getProductById);
 
 // POST a new product
-router.post("/", async (req, res) => {
-  if (!res) {
-    res.status(500).send("Error");
-  }
+router.post("/", productsController.postNewProduct);
 
-  // First round of validation with Joi
-  const joiValidation = productSchemaJoi.validate(req.body);
-  if (joiValidation.error) {
-    res.status(400).send({
-      error: `${
-        joiValidation.error.name
-      } (Joi): ${joiValidation.error.details.map((err) => {
-        return err.message;
-      })}`,
-    });
-    return;
-  }
-  console.log("Joi validation successful");
-
-  // Creating new mongoose product with body
-  const product = new Product(req.body);
-  // Second round of validation with Mongoose
-  await product.validate();
-  console.log("Mongoose validation successful");
-
-  // Saving in DB and sending result
-  const result = await product.save();
-  res.send({ insertedCount: 1, result: result });
-});
-
-// PUT a specific product (with ID)
-router.put("/:id", validateObjectId, async (req, res) => {
-  if (!res) {
-    res.status(500).send("Error");
-  }
-
-  // Add a modification date
-  req.body.modificationDate = new Date();
-
-  // First round of validation with Joi
-  const joiValidation = productSchemaJoi.validate(req.body);
-  if (joiValidation.error) {
-    res.status(400).send({
-      error: `${
-        joiValidation.error.name
-      } (Joi): ${joiValidation.error.details.map((err) => {
-        return err.message;
-      })}`,
-    });
-    return;
-  }
-  console.log("Joi validation successful");
-
-  const updatedProduct = new Product(req.body);
-  // Validation with Mongoose
-  await updatedProduct.validate();
-
-  // Finding and updating at the same time
-  const result = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!result) {
-    throw err;
-  }
-  res.send({ updatedCount: 1, updatedObj: result });
-});
+// PUT a specific product (with ID) (whole product)
+router.put("/:id", validateUuid, productsController.putProductWithId);
 
 // DELETE a specific order (with ID)
-router.delete("/:id", async (req, res) => {
-  if (!res) {
-    res.status(500).send("Error");
-  }
-
-  const result = await Product.remove({ _id: req.params.id });
-  if (result.deletedCount === 0) {
-    res.status(404).send("Product not found");
-  }
-  res.send({ deletedCount: 1, result: result });
-});
+router.delete("/:id", validateUuid, productsController.deleteProductWithId);
 
 module.exports = router;
