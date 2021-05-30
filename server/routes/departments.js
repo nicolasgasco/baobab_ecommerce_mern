52399ff94b64866f548df32fbecb2e544bc88504
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-30 22:06:09 +0200

    Moved routes logic to external controllers

--- server/routes/departments.js
+++ server/routes/departments.js
@@ -1,56 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { departmentSchema, Department } = require("../models/departments");
+const departmentsController = require("../controllers/departments");
 
 const validateObjectId = require("../middleware/validateObjectId");
 
 // GET all departments of a specific language
-router.get("/", async (req, res) => {
-  let language = req.query.lang;
-
-  if (!res) {
-    res.status(500).send("Error");
-  }
-
-  // Showing only the desired language
-  let filter;
-  if (language) {
-    filter = { _id: 1 };
-    filter[`translations.${language}`] = 1;
-  }
-
-  // Database request
-  const departments = await Department.find()
-    .sort(`translations.${language}`)
-    .select(filter);
-  console.log();
-  if (departments.length === 0) {
-    return res.status(404).send({ error: "Nothing found" });
-  } else {
-    res.send({
-      resultsFound: departments.length,
-      sortBy: language,
-      results: departments,
-    });
-  }
-});
+router.get("/", departmentsController.getLocalizedDepartments);
 
 // GET a specific department (used for validation too)
-router.get("/:id", validateObjectId, async (req, res) => {
-  if (!res) res.status(500).send("Error");
-
-  let id = req.params.id;
-
-  // Database request
-  const department = await Department.find({ _id: id });
-  if (!department[0]) {
-    return res.status(404).send({ error: "Department not found" });
-  }
-  res.send({
-    resultsFound: 1,
-    result: department[0],
-  });
-});
+router.get("/:id", validateObjectId, departmentsController.getDepartmentById);
 
 module.exports = router;
