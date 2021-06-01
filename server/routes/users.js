db279213bd336ae0544edbcd32b71a32839668dc
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-01 12:47:01 +0200

    Created controller for users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,112 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { encryptPassword } = require("../middleware/encryptPassword");
 
-// Data validation with joi
-const Joi = require("joi");
-const userSchemaJoi = require("../joi/users");
+const usersController = require("../controllers/users");
 
+// Middleware
 const validateObjectId = require("../middleware/validateObjectId");
 
-// Initializing users
-const { userSchema, User } = require("../models/users");
+
 
 // GET all users
-router.get("/", async (req, res) => {
-  // Query param for sorting, by which and in which order
-  let sortBy = req.query.sortBy;
-  const order = req.query.order;
-
-  // You can add a - in front of key for descending order
-  sortBy = order === "-1" ? "-" + sortBy : sortBy;
-
-  // Database request
-  const users = await User.find().sort(sortBy);
-  res.send({
-    resultsFound: users.length,
-    sortBy: () => {
-      return sortBy ? sortBy.replace("-", "") : null;
-    },
-    order,
-    results: users,
-  });
-});
+router.get("/", usersController.getAllUsers);
 
 // POST a new user
-router.post("/", async (req, res) => {
-  // First round of validation with Joi
-  const joiValidation = userSchemaJoi.validate(req.body);
-  if (joiValidation.error) {
-    res.status(400).send({
-      error: `${
-        joiValidation.error.name
-      } (Joi): ${joiValidation.error.details.map((err) => {
-        return err.message;
-      })}`,
-    });
-    return;
-  }
-  console.log("Joi validation successful");
-
-  // Encrypt password now and not before, otherwise it cannot be validated
-  req.body.password = encryptPassword(req.body.password);
-
-  // Creating new mongoose user with body
-  const user = new User(req.body);
-
-  // Second round of validation with Mongoose
-  await user.validate();
-  console.log("Mongoose validation successful");
-
-  // Saving in DB and sending result
-  const result = await user.save();
-  res.send({ insertedCount: 1, result: result });
-});
+router.post("/", usersController.postNewUser);
 
 // PUT a specific user (with ID)
-router.put("/:id", validateObjectId, async (req, res) => {
-  // Add a modification date
-  req.body.modificationDate = new Date();
-
-  // First round of validation with Joi
-  const joiValidation = userSchemaJoi.validate(req.body);
-  if (joiValidation.error) {
-    res.status(400).send({
-      error: `${
-        joiValidation.error.name
-      } (Joi): ${joiValidation.error.details.map((err) => {
-        return err.message;
-      })}`,
-    });
-    return;
-  }
-  console.log("Joi validation successful");
-
-  const updatedUser = new User(req.body);
-
-  // Validation with Mongoose
-  await updatedUser.validate();
-
-  req.body.password = updatedUser.password
-    ? encryptPassword(req.body.password)
-    : null;
-  // Finding and updating at the same time
-  const result = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!result) {
-    throw err;
-  }
-  res.send({ updatedCount: 1, updatedObj: result });
-});
+router.put("/:id", validateObjectId, usersController.putUserWithId);
 
 // DELETE a specific user (with ID)
-router.delete("/:id", validateObjectId, async (req, res) => {
-  const result = await User.remove({ _id: req.params.id });
-  if (result.deletedCount === 0) {
-    res.status(404).send("Product not found");
-  }
-  res.send({ deletedCount: 1, result: result });
-});
+router.delete("/:id", validateObjectId, usersController.deleteUser);
 
 module.exports = router;
