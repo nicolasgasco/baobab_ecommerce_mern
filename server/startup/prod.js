84c05c4acdd1f2579b92e067a696a8c319817712
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-01 12:49:37 +0200

    Fixed typo

--- server/startup/prod.js
+++ server/startup/prod.js
@@ -2,6 +2,6 @@ const helmet = require("helmet");
 const compression = require("compression");
 
 module.exports = function (app) {
-  app.use(helmete());
+  app.use(helmet());
   app.use(compression());
 };
