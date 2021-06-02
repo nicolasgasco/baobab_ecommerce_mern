cf3f77257ec3d49f46331cc4b45c081545452c25
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-02 17:18:36 +0200

    Optmized routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+// All logic is here
 const usersController = require("../controllers/users");
+const joiValidationUsers = require("../middleware/joiValidationUsers");
 
-// Middleware
+// Middleware validation
 const validateObjectId = require("../middleware/validateObjectId");
-
-
 
 // GET all users
 router.get("/", usersController.getAllUsers);
 
+// GET user by ID
+router.get("/:id", validateObjectId, usersController.getUserById);
+
 // POST a new user
-router.post("/", usersController.postNewUser);
+router.post("/", joiValidationUsers, usersController.postNewUser);
 
 // PUT a specific user (with ID)
 router.put("/:id", validateObjectId, usersController.putUserWithId);
