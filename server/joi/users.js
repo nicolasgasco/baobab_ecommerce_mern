7026ae31759dd8b36a5b0101fec3409bd8013c2e
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-02 17:21:27 +0200

    Optimized schema

--- server/joi/users.js
+++ server/joi/users.js
@@ -2,15 +2,15 @@ const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
 
 const userSchemaJoi = Joi.object({
-  name: Joi.string().min(2).max(25).trim(),
-  surname: Joi.string().min(2).max(25).trim(),
-  password: passwordComplexity().trim(),
+  name: Joi.string().min(2).max(25).trim().required(),
+  surname: Joi.string().min(2).max(25).trim().required(),
+  password: passwordComplexity().trim().required(),
   email: Joi.when("modificationDate", {
     is: null,
     then: Joi.optional(),
-    otherwise: Joi.string().lowercase().email(),
+    otherwise: Joi.string().lowercase().email().required(),
   }),
-  gender: Joi.string().valid("f", "m", "o").trim(),
+  gender: Joi.string().valid("f", "m", "o").trim().required(),
   birthday: Joi.date()
     .max(
       `${new Date().getMonth() + 1}-${new Date().getDate()}-${
@@ -22,21 +22,22 @@ const userSchemaJoi = Joi.object({
         new Date().getFullYear() - 125
       }`
     )
-    ,
+    .required(),
   creationDate: Joi.date().iso().default("now"),
   modificationDate: Joi.date().iso().less("now"),
   address: {
     countryCode: Joi.string()
       .pattern(new RegExp("^[A-Z]{3}$"))
       .uppercase()
-      .trim(),
-    region: Joi.string().trim().min(2).max(25),
-    province: Joi.string().trim().min(2).max(25),
-    city: Joi.string().trim().min(2).max(25),
-    zip: Joi.string().trim().length(5).regex(/^\d+$/),
-    street: Joi.string().trim().min(1).max(25),
-    streetNumber: Joi.string().trim().min(1).max(10),
-    doorNumber: Joi.string().trim().min(1).max(10),
+      .trim()
+      .required(),
+    region: Joi.string().trim().min(2).max(25).required(),
+    province: Joi.string().trim().min(2).max(25).required(),
+    city: Joi.string().trim().min(2).max(25).required(),
+    zip: Joi.string().trim().length(5).regex(/^\d+$/).required(),
+    street: Joi.string().trim().min(1).max(25).required(),
+    streetNumber: Joi.string().trim().min(1).max(10).required(),
+    doorNumber: Joi.string().trim().min(1).max(10).required(),
     other: Joi.string().trim().min(2).max(50),
   },
   telephone: {
@@ -44,10 +45,16 @@ const userSchemaJoi = Joi.object({
       .trim()
       .min(2)
       .max(4)
-      .pattern(new RegExp("^\\+[0-9]{1,6}$")),
-    number: Joi.string().trim().min(4).max(15).pattern(new RegExp("^[0-9]*$")),
+      .pattern(new RegExp("^\\+[0-9]{1,6}$"))
+      .required(),
+    number: Joi.string()
+      .trim()
+      .min(4)
+      .max(15)
+      .pattern(new RegExp("^[0-9]*$"))
+      .required(),
   },
-  tier: Joi.number().valid(0, 1),
+  tier: Joi.number().valid(0, 1).required(),
 });
 
 module.exports = userSchemaJoi;
