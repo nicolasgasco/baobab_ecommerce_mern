862d3d7152bd80dc0d59228026755a373619b68e
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-02 17:21:09 +0200

    Added error message

--- server/middleware/validateObjectId.js
+++ server/middleware/validateObjectId.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = (req, res, next) => {
+  if (!req.params.id) res.status(400).send({ error: "Id missing" });
   if (!mongoose.Types.ObjectId.isValid(req.params.id))
     return res.status(400).send({ error: "Invalid ObjectId" });
   next();
