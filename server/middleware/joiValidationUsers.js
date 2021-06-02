b10f2273e28b3fa8e339cc1e6361b1be15783f49
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-02 17:19:00 +0200

    Created separated middleware for Products and Users

---
+++ server/middleware/joiValidationUsers.js
@@ -0,0 +1,16 @@
+const userSchemaJoi = require("../joi/users");
+
+module.exports = (req, res, next) => {
+  const joiValidation = userSchemaJoi.validate(req.body);
+  if (joiValidation.error) {
+    res.status(400).send({
+      error: `${
+        joiValidation.error.name
+      } (Joi): ${joiValidation.error.details.map((err) => {
+        return err.message;
+      })}`,
+    });
+    return;
+  }
+  next();
+};
