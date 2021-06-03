ed494c15c90cb18795d2b17d9a15c4ca854875ef
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-03 18:12:44 +0200

    Added picture validation

--- server/joi/products.js
+++ server/joi/products.js
@@ -9,12 +9,32 @@ const productSchemaJoi = Joi.object({
     productDesc1: Joi.string().lowercase().min(2).max(50).trim().required(),
     productDesc2: Joi.string().lowercase().min(2).max(50).trim().required(),
     productDesc3: Joi.string().lowercase().min(2).max(50).trim().required(),
+    color: Joi.string().lowercase().min(2).max(10).trim().required(),
     productGender: Joi.string()
       .lowercase()
       .valid("unisex", "male", "female")
       .trim()
       .required(),
   },
+  pictures: Joi.array().items({
+    url: Joi.string()
+      .min(10)
+      .max(500)
+      .trim()
+      .pattern(
+        /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/
+      )
+      .lowercase()
+      .required(),
+    alt: Joi.string().min(5).max(50).lowercase().trim().required()
+      }),
+  completeNameDesc: Joi.string()
+    .min(20)
+    .max(230)
+    .trim()
+    .pattern(/[A-Za-z0-9]+/)
+    .lowercase()
+    .required(),
   productId: Joi.string().guid(),
   creationDate: Joi.date().iso().default("now"),
   modificationDate: Joi.date().iso().less("now"),
