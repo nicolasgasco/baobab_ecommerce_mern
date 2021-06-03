6adb4d52394872cbd2b02a92c7aeea6ae11edf1f
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-03 18:10:30 +0200

    Added route to fetch products by keywords

--- server/routes/products.js
+++ server/routes/products.js
@@ -6,15 +6,20 @@ const productsController = require("../controllers/products");
 // Middleware
 const joiValidationProducts = require("../middleware/joiValidationProducts");
 const validateUuid = require("../middleware/validateUuid");
+const createCompleteName = require("../middleware/createCompleteName");
+const createKeywords = require("../middleware/createKeywords");
 
 // GET all products
 router.get("/", productsController.getAllProducts);
+
+// GET products filtering per keywords 
+router.get("/search", createKeywords, productsController.getProductsByKeywords);
 
 //  GET a specific product with id
 router.get("/:id", validateUuid, productsController.getProductById);
 
 // POST a new product
-router.post("/", joiValidationProducts, productsController.postNewProduct);
+router.post("/", [createCompleteName, joiValidationProducts], productsController.postNewProduct);
 
 // PUT a specific product (with ID) (whole product)
 router.put("/:id", [joiValidationProducts, validateUuid], productsController.putProductWithId);
