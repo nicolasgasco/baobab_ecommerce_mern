025fa81dffc0f3ceca4293b25c3627828efcc94e
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-05 00:11:00 +0200

    Added a loading animation for a whole section

--- client/src/components/MainContent/ResultsBox.js
+++ client/src/components/MainContent/ResultsBox.js
@@ -1,17 +1,24 @@
 import NothingFound from "../UI/NothingFound";
 import ProductCard from "../UI/ProductCard";
+import SectionLoading from "../UI/SectionLoading";
 
 const ResultsBox = (props) => {
   const showProducts = props.fetchedProducts.map((product) => {
-    return <ProductCard product={product} isLoading={props.isLoading} />;
+    return (
+      <ProductCard product={product} picturesLoading={props.picturesLoading} />
+    );
   });
-
-  console.log(props.isEmpty);
 
   return (
     <div className="w-auto my-10 bg-gray-200 flex justify-center content-center">
-      <div className="bg-white w-11/12 md:w-10/12 m-6 bg-white p-6 rounded-lg shadow-lg flex flex-wrap justify-center">
-        {props.isEmpty ? <NothingFound /> : showProducts}
+      <div className="bg-white w-11/12 md:w-10/12 m-6 p-6 rounded-lg shadow-lg flex flex-wrap justify-center">
+        {props.isEmpty ? (
+          <NothingFound />
+        ) : props.contentLoading ? (
+          <SectionLoading />
+        ) : (
+          showProducts
+        )}
       </div>
     </div>
   );
