bdc8a7f1952e9a9da1091766bcc6af9e8827889b
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-05 00:11:26 +0200

    Added state for loading pictures and contents

--- client/src/components/MainContent/MainContent.js
+++ client/src/components/MainContent/MainContent.js
@@ -1,13 +1,24 @@
 import { useEffect, useState } from "react";
+import SectionLoading from "../UI/SectionLoading";
 import ResultsBox from "./ResultsBox";
 import StoreSearchbar from "./StoreSearchbar";
 
 const MainContent = () => {
   const [fetchedProducts, setFecthedProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isEmpty, setIsEmpty] = useState(true);
+  const [contentLoading, setContentLoading] = useState(false);
+  // Used for when pictures are still loading
+  const [picturesLoading, setPicturesLoading] = useState(false);
+  // Used to show message when there are no results to show
+  const [isEmpty, setIsEmpty] = useState(false);
 
+  // Fetching the products by keywords
   const getSearchbarInput = (input) => {
+    setContentLoading(true);
+    setPicturesLoading(true);
+    setIsEmpty(false);
+    // THis is necessary, otherwise some products just stay there
+    setFecthedProducts([]);
+
     fetch("api/products/search", {
       method: "POST",
       headers: {
@@ -17,9 +28,17 @@ const MainContent = () => {
     })
       .then((res) => res.json())
       .then((res) => {
-        setFecthedProducts(res.results);
-        setIsLoading(false);
-        res.results.length === 0 ? setIsEmpty(true) : setIsEmpty(false);
+          console.log(res.results.length)
+        if (res.results.length === 0) {
+          setIsEmpty(true);
+          setPicturesLoading(false);
+          setContentLoading(false);
+        } else {
+          setFecthedProducts(res.results);
+          setPicturesLoading(false);
+          setContentLoading(false);
+          setIsEmpty(false);
+        }
       })
       .catch((error) => {
         console.log("An error ocurred:" + error.message);
@@ -31,7 +50,8 @@ const MainContent = () => {
       <StoreSearchbar onGetSearchbarInput={getSearchbarInput} />
       <ResultsBox
         fetchedProducts={fetchedProducts}
-        isLoading={isLoading}
+        contentLoading={contentLoading}
+        picturesLoading={picturesLoading}
         isEmpty={isEmpty}
       />
     </>
