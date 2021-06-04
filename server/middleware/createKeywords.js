c99dfc0ec0705a0a7aa73d3e003055b6e295f1ae
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-04 20:40:05 +0200

    Sending error if keywords is empty string

--- server/middleware/createKeywords.js
+++ server/middleware/createKeywords.js
@@ -4,16 +4,23 @@ const encryptPassword = (password) => {
 };
 
 module.exports = (req, res, next) => {
-  // Creating list of lowercase words
-  const keywordsList = req.body.keywords
-    .split(" ")
-    .filter((word) => {
-      return /[A-Za-z0-9]+/.test(word);
-    })
-    .map((word) => {
-      return word.toLowerCase();
-    });
+  // Check if keywords contains at least a letter
+  const regExp = /[a-zA-Z]+/;
+  if (regExp.test(req.body.keywords)) {
+    // Creating list of lowercase words
+    const keywordsList = req.body.keywords
+      .split(" ")
+      .filter((word) => {
+        return /[A-Za-z0-9]+/.test(word);
+      })
+      .map((word) => {
+        return word.toLowerCase();
+      });
 
-  req.body.keywords = keywordsList.join("|");
+    req.body.keywords = keywordsList.join("|");
+  } else {
+    return res.status(400).send({ error: "Invalid keywords" });
+  }
+
   next();
 };
