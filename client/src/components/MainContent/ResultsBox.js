3d14eaf05d99bcf9ed6bc61456f96b9182ed8ae9
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-06 23:15:41 +0200

    Added styling and keys

--- client/src/components/MainContent/ResultsBox.js
+++ client/src/components/MainContent/ResultsBox.js
@@ -1,26 +1,26 @@
 import NothingFound from "../UI/NothingFound";
-import ProductCard from "../UI/ProductCard";
+import ProductCard from "../UI/ProductCard1";
 import SectionLoading from "../UI/SectionLoading";
 
 const ResultsBox = (props) => {
   const showProducts = props.fetchedProducts.map((product) => {
     return (
-      <ProductCard product={product} picturesLoading={props.picturesLoading} />
+      <ProductCard key={`product-${product._id}`} product={product} picturesLoading={props.picturesLoading} />
     );
   });
 
   return (
-    <div className="w-auto my-10 bg-gray-200 flex justify-center content-center">
-      <div className="bg-white w-11/12 md:w-10/12 m-6 p-6 rounded-lg shadow-lg flex flex-wrap justify-center">
+    <section className="w-auto my-10 bg-gray-200 flex justify-center content-center">
+      <div className="bg-white  mw-11/12 md:w-10/12 m-6 p-6 rounded-lg shadow-lg flex flex-wrap justify-center">
         {props.isEmpty ? (
           <NothingFound />
         ) : props.contentLoading ? (
-          <SectionLoading />
+          <SectionLoading className="w-8/12" />
         ) : (
           showProducts
         )}
       </div>
-    </div>
+    </section>
   );
 };
 
