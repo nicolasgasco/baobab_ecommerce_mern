ababdab0b5512d8db6c33f47ec602d8adcd43744
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-06 23:15:24 +0200

    Added ref to input searchbar

--- client/src/components/MainContent/StoreSearchbar.js
+++ client/src/components/MainContent/StoreSearchbar.js
@@ -1,29 +1,31 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 const StoreSearchbar = (props) => {
-
+  const searchbarInputRef = useRef();
 
   const handleSubmitSearchbar = (event) => {
     event.preventDefault();
-    const searchbarInput = event.target.children[0].value; 
+    const searchbarInput = searchbarInputRef.current.value;
     if (searchbarInput) props.onGetSearchbarInput(searchbarInput.trim());
   };
 
   return (
-    <div className="py-2">
-      <form onSubmit={handleSubmitSearchbar} className="bg-gray-200 bg-opacity-60 w-11/12 lg:w-1/2 max-w-full mx-auto flex items-center rounded-full shadow-xl">
-        <input
-          className="rounded-l-full bg-transparent w-full py-4 px-6 text-gray-700 placeholder-gray-700 text-center leading-tight focus:outline-none tracking-wider text-lg"
-          id="search"
-          type="text"
-          placeholder="Search..."
-        />
+    <form
+      onSubmit={handleSubmitSearchbar}
+      className="py-2 bg-gray-200 bg-opacity-60 w-11/12 lg:w-1/2 max-w-full mx-auto flex items-center rounded-full shadow-xl"
+    >
+      <input
+        className="rounded-l-full bg-transparent w-full py-4 px-6 text-gray-700 placeholder-gray-700 text-center leading-tight focus:outline-none tracking-wider text-lg"
+        id="search"
+        type="text"
+        placeholder="Search..."
+        ref={searchbarInputRef}
+      />
 
-        <div className="p-2">
-          <button className="search-icon bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-10 h-10 flex items-center justify-center" />
-        </div>
-      </form>
-    </div>
+      <div className="p-2">
+        <button className="search-icon bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-10 h-10 flex items-center justify-center" />
+      </div>
+    </form>
   );
 };
 
