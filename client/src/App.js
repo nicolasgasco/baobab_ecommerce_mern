e49a6a0dec9f05761a9a30a221ced83141a18c95
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-06 23:16:21 +0200

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MainNav from "./components/UI/MainNav";
 import HeaderBanner from "./components/UI/HeaderBanner";
 import MainModal from "./components/UI/MainModal";
 import VideoBackground from "./components/UI/VideoBackground";
 import MainContent from "./components/MainContent/MainContent";
+import HeroMain from "./components/UI/HeroMain";
+import Footer from "./components/UI/Footer";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <HeaderBanner />
       <MainModal />
-
       <VideoBackground />
-      <MainNav />
-      <MainContent />
-    </>
+
+      <Switch>
+        <Route exact path="/">
+          <div>
+            <MainNav />
+            <MainContent />
+          </div>
+        </Route>
+      </Switch>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
