e1418dcff093850fc9f726f8551d2f46f09d40a3
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-07 21:28:24 +0200

    Added dropdown for number of results per page

--- client/src/components/MainContent/ResultsBox.js
+++ client/src/components/MainContent/ResultsBox.js
@@ -2,6 +2,7 @@ import NothingFound from "../UI/NothingFound";
 import PageCounter from "../UI/PageCounter";
 import ProductCard from "../UI/ProductCard1";
 import SectionLoading from "../UI/SectionLoading";
+import SimpleDropdown from "../UI/SimpleDropdown";
 
 const ResultsBox = (props) => {
   const showProducts = props.fetchedProducts.map((product) => {
@@ -14,6 +15,8 @@ const ResultsBox = (props) => {
     );
   });
 
+  const resultsPerPageDropdown = ["4", "8", "24", "60"];
+
   return (
     <section className="p-5 my-10 bg-gray-200">
       <div className="bg-white mx-auto mw-11/12 md:w-10/12 m-6 p-6 rounded-lg shadow-lg">
@@ -23,12 +26,17 @@ const ResultsBox = (props) => {
           <SectionLoading className="w-8/12" />
         ) : (
           <>
+            <div className="bg-black flex-wrap justify-end">
+              <SimpleDropdown
+                heading="Results per page"
+                fields={resultsPerPageDropdown}
+              />
+            </div>
             <div className="flex flex-wrap justify-center">{showProducts}</div>
             <PageCounter
               paginationData={props.paginationData}
               className="flex justify-center"
               handlePageChange={props.handlePageChange}
-
             />
           </>
         )}
---
+++ client/src/components/UI/SimpleDropdown.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const SimpleDropdown = ({ heading, fields }) => {
+  const [show, setShow] = useState(true);
+
+  const showFields = fields.map((field) => {
+    return (
+      <li className="cursor-pointer text-gray-600 dark:text-gray-400 dark:hover:bg-gray-700 text-sm leading-3 tracking-normal py-3 hover:bg-gray-100 px-3 font-normal">
+        {field}
+      </li>
+    );
+  });
+
+  return (
+    <div className="mx-auto">
+      <div className="container flex justify-center mx-auto py-2">
+        <div className=" mb-2">
+          {/* Code block starts */}
+          <div className="relative">
+            <div
+              className="bg-white dark:bg-gray-800 flex items-center justify-between border rounded border-gray-300 dark:border-gray-700 w-40 cursor-pointer"
+              onClick={() => setShow(!show)}
+            >
+              <p className="pl-3 py-3 text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal font-normal">
+                {heading}
+              </p>
+              <div className="cursor-pointer text-gray-600 dark:text-gray-400 mr-3">
+                {show ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className=" icon icon-tabler icon-tabler-chevron-up"
+                    width={20}
+                    height={20}
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <polyline points="6 9 12 15 18 9" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-chevron-up"
+                    width={20}
+                    height={20}
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <polyline points="6 15 12 9 18 15" />
+                  </svg>
+                )}
+              </div>
+            </div>
+            {show && (
+              <ul className="visible transition duration-300 opacity-100 bg-white dark:bg-gray-800 shadow rounded mt-2 w-48 py-1 absolute">
+                {showFields}
+              </ul>
+            )}
+          </div>
+          {/* Code block ends */}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SimpleDropdown;
