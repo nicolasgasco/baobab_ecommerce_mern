733142e2d98a7aa3982f1c1408f9c3f1379f3b07
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-09 00:02:27 +0200

    Tweaked styles

--- client/src/components/MainContent/StoreSearchbar.js
+++ client/src/components/MainContent/StoreSearchbar.js
@@ -13,11 +13,11 @@ const StoreSearchbar = ({ onGetSearchbarInput, handleActivePage }) => {
       handleActivePage(1);
     }
   };
-  
+
   return (
     <form
       onSubmit={handleSubmitSearchbar}
-      className="bg-gray-200 bg-opacity-60 w-11/12 lg:w-1/2 max-w-full mx-auto flex items-center rounded-full shadow-xl"
+      className="bg-white bg-opacity-100 w-11/12 lg:w-1/2 max-w-full mx-auto flex items-center rounded-full shadow-xl"
     >
       <input
         className="rounded-l-full bg-transparent w-full py-4 px-6 text-gray-700 placeholder-gray-700 text-center leading-tight focus:outline-none tracking-wider text-lg"
@@ -28,7 +28,7 @@ const StoreSearchbar = ({ onGetSearchbarInput, handleActivePage }) => {
       />
 
       <div className="p-2">
-        <button className={`${classes["search-button"]} bg-yellow-500 text-white rounded-full p-2 hover:bg-yellow-400 focus:outline-none w-10 h-10 flex items-center justify-center`} />
+        <button className={`${classes["search-button"]} bg-green-500 text-white rounded-full p-2 hover:bg-green-400 focus:outline-none w-10 h-10 flex items-center justify-center`} />
       </div>
     </form>
   );
