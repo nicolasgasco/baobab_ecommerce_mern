5e9f64f317c132732be2e43292e8110c0110b42e
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-09 23:36:49 +0200

    Added Auth context

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MainNav from "./components/Header/MainNav";
 import HeaderBanner from "./components/Header/HeaderBanner";
 import MainModal from "./components/UI/MainModal";
-import VideoBackground from "./components/Header/VideoBackground";
-import MainContent from "./components/MainContent/MainContent";
 import Footer from "./components/Footer/Footer";
+import AuthProvider from "./store/AuthProvider";
+import MainContent from "./components/MainContent/MainContent";
 
 function App() {
   return (
     <BrowserRouter>
       <HeaderBanner />
-      <MainModal />
-      {/* <VideoBackground /> */}
 
-      <Switch>
-        <Route exact path="/">
-          <div className="h-min-screen">
-            <MainNav />
-            <MainContent />
-          </div>
-        </Route>
-      </Switch>
+      <AuthProvider>
+        <MainModal />
+        {/* <VideoBackground /> */}
+
+        <Switch>
+          <Route exact path="/">
+            <div className="h-min-screen">
+              <MainNav />
+              <MainContent />
+            </div>
+          </Route>
+        </Switch>
+      </AuthProvider>
 
       <Footer />
     </BrowserRouter>
