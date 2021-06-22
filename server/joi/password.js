379ed97a49705775d83e0c5eeccc3f26f6ccc92e
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-22 23:48:55 +0200

    Preparing app for deployment

--- server/joi/password.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-const passwordComplexity = require("joi-password-complexity");
-
-const passwordSchemaJoi = Joi.object({
-  password: passwordComplexity().required(),
-});
-
-module.exports = passwordSchemaJoi;
--- server/middleware/encryptPassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-
-// Using actual function over middleware because I want to validate password before sending it database
-const encryptPassword = (password) => {
-  const saltRounds = process.env.SALT;
-  return bcrypt.hashSync(password, +saltRounds);
-};
-
-module.exports.encryptPassword = encryptPassword;
