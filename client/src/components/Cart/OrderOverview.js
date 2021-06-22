76fcfba1c000ef8dce594916121dd705a48ed386
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-22 12:40:36 +0200

    Working on system for rating products in the orders overview

--- client/src/components/Cart/OrderOverview.js
+++ client/src/components/Cart/OrderOverview.js
@@ -1,38 +1,107 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import { StarIcon } from "@heroicons/react/outline";
+import RatingSystem from "./RatingSystem";
 
 const OrderOverview = ({ items, classes }) => {
+  const [showRatingSystem, setShowRatingSystem] = useState(false);
+  const [currentItem, setCurrentItem] = useState("");
+  const [ratingValue, setRatingValue] = useState(0);
+  const [showSuccessRating, setShowSuccessRating] = useState(false);
+
+  useEffect(() => {
+    setRatingValue(0);
+  }, []);
+
+  const handleRateButton = (id) => {
+    setRatingValue(0);
+    setShowRatingSystem((prevState) => {
+      return !prevState;
+    });
+    setCurrentItem(id);
+  };
+
+  const getRatingValue = (value) => {
+    console.log(value, "value is");
+    setRatingValue(value);
+  };
+  const handleSubmitRating = () => {
+    console.log(ratingValue);
+  };
+
   const showCartItems = items.map((item, index) => {
     return (
-      <tr className={`h-24 border-gray-300 dark:border-gray-200 border-b `}>
-        <td className="hidden md:table-cell text-sm whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
-          <img
-            src={item.pictures[0].url}
-            className="w-16 h-16 mx-auto object-contain rounded-full ring-2 ring-green-500"
-            alt={item.pictures[0].alt}
-          />
-        </td>
-        <td className="text-sm whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
-          <p className="mb-2 md:ml-4">{`${item.completeName.brand} ${item.completeName.productName}, ${item.completeName.color}, ${item.completeName.productGender}`}</p>
-        </td>
+      <>
+        <tr className={`h-24 border-gray-300 dark:border-gray-200 border-b `}>
+          {/* Image */}
+          <td className="hidden md:table-cell text-sm whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+            <img
+              src={item.pictures[0].url}
+              className="w-16 h-16 mx-auto object-contain rounded-full ring-2 ring-green-500"
+              alt={item.pictures[0].alt}
+            />
+          </td>
+          {/* Description */}
+          <td className="text-sm whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+            <p className="mb-2 md:ml-4">{`${item.completeName.brand} ${item.completeName.productName}, ${item.completeName.color}, ${item.completeName.productGender}`}</p>
+          </td>
+          {/* Quantity */}
+          <td className="text-center text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+            <div className="relative flex flex-row justify-center w-12 h-8">
+              <p>{item.quantity}</p>
+            </div>
+          </td>
+          {/* Unit price */}
+          <td className="pr-4 whitespace-no-wrap  hidden md:table-cell">
+            <div className="text-center">
+              <span className="text-xs lg:text-base font-medium">{`${
+                item.pricingInfo.price
+              }${String.fromCharCode(160)}€`}</span>
+            </div>
+          </td>
+          {/* Total price */}
+          <td className="text-center text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+            <span className="text-sm lg:text-base font-medium">{`${(
+              item.pricingInfo.price * item.quantity
+            ).toFixed(2)}${String.fromCharCode(160)}€`}</span>
+          </td>
+          {/* Rate product */}
+          <td className="text-center text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+            <span className="sr-only">Rate product</span>
+            <StarIcon
+              onClick={() => handleRateButton(item._id)}
+              className="h-6 cursor-pointer"
+            />
+          </td>
+        </tr>
 
-        <td className="text-center  text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
-          <div className="relative flex flex-row w-12 h-8">
-            <p>{item.quantity}</p>
-          </div>
-        </td>
-        <td className="pr-4 whitespace-no-wrap">
-          <div className="text-center">
-            <span className="text-xs lg:text-base font-medium">{`${
-              item.pricingInfo.price
-            }${String.fromCharCode(160)}€`}</span>
-          </div>
-        </td>
-        <td className="text-center text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
-          <span className="text-sm lg:text-base font-medium">{`${(
-            item.pricingInfo.price * item.quantity
-          ).toFixed(2)}${String.fromCharCode(160)}€`}</span>
-        </td>
-      </tr>
+        {showRatingSystem && currentItem === item._id && (
+          <tr className={`h-16 border-gray-300 dark:border-gray-200 border-b `}>
+            <td
+              colSpan={6}
+              className="text-sm whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4"
+            >
+              <div className="flex justify-center">
+                {showSuccessRating ? (
+                  "Your rating was saved."
+                ) : (
+                  <>
+                    {" "}
+                    <RatingSystem getRatingValue={getRatingValue} />
+                    <button
+                      onClick={handleSubmitRating}
+                      type="button"
+                      className="inline-flex justify-center py-2 px-4 my-2  ml-3 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                    >
+                      Rate
+                    </button>
+                  </>
+                )}
+              </div>
+            </td>
+          </tr>
+        )}
+      </>
     );
   });
 
@@ -49,11 +118,14 @@ const OrderOverview = ({ items, classes }) => {
               <th className="w-1/12  text-center text-gray-600 dark:text-gray-400 font-bold pr-6 text-sm tracking-normal leading-4">
                 Quantity
               </th>
-              <th className="w-1/12  text-center text-gray-600 dark:text-gray-400 font-bold pr-6 text-sm tracking-normal leading-4">
+              <th className="w-1/12  text-center hidden md:table-cell text-gray-600 dark:text-gray-400 font-bold pr-6 text-sm tracking-normal leading-4">
                 Unit
               </th>
               <th className="w-2/12  text-center text-gray-600 dark:text-gray-400 font-bold pr-8 text-sm tracking-normal leading-4">
                 Total
+              </th>
+              <th className="w-2/12  text-center text-gray-600 dark:text-gray-400 font-bold pr-8 text-sm tracking-normal leading-4">
+                Rate
               </th>
             </tr>
           </thead>
---
+++ client/src/components/Cart/RatingSystem.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+const RatingSystem = ({ classes, getRatingValue }) => {
+  const [starRating, setStarRating] = useState(0);
+  const [hoverStarRating, setHoverStarRating] = useState(0);
+
+  useEffect(() => {
+    setStarRating(0);
+  }, []);
+
+  const handleMouseEnter = (value) => {
+    setHoverStarRating(value);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverStarRating(0);
+  };
+
+  const determineFillColor = (value) => {
+    if (hoverStarRating) {
+      return hoverStarRating >= value ? "#FCD34D" : "#9CA3AF";
+    } else {
+      return starRating >= value ? "#F59E0B" : "#9CA3AF";
+    }
+  };
+
+  const handleOnClick = (value) => {
+    setStarRating(value);
+    getRatingValue(value);
+  };
+
+  const showStars = [1, 2, 3, 4, 5].map((number) => {
+    return (
+      <div
+        className="pr-4 lg:pr-2"
+        onMouseEnter={() => handleMouseEnter(number)}
+        onMouseLeave={handleMouseLeave}
+        onClick={() => handleOnClick(number)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={18}
+          height={17}
+          viewBox="0 0 14 13"
+          fill="none"
+        >
+          <path
+            d="M6.52447 0.463524C6.67415 0.00286841 7.32585 0.00286996 7.47553 0.463525L8.68386 4.18237C8.75079 4.38838 8.94277 4.52786 9.15938 4.52786H13.0696C13.554 4.52786 13.7554 5.14767 13.3635 5.43237L10.2001 7.73075C10.0248 7.85807 9.95149 8.08375 10.0184 8.28976L11.2268 12.0086C11.3764 12.4693 10.8492 12.8523 10.4573 12.5676L7.29389 10.2693C7.11865 10.1419 6.88135 10.1419 6.70611 10.2693L3.54267 12.5676C3.15081 12.8523 2.62357 12.4693 2.77325 12.0086L3.98157 8.28976C4.04851 8.08375 3.97518 7.85807 3.79994 7.73075L0.636495 5.43237C0.244639 5.14767 0.446028 4.52786 0.93039 4.52786H4.84062C5.05723 4.52786 5.24921 4.38838 5.31614 4.18237L6.52447 0.463524Z"
+            fill={determineFillColor(number)}
+          />
+        </svg>
+      </div>
+    );
+  });
+
+  return <div className={`flex items-center ${classes}`}>{showStars}</div>;
+};
+
+export default RatingSystem;
