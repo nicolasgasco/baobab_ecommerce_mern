7ded47cfb0a8da1dfde5162e8428d22a078e1349
nicolasgasco <nicolas.gasco92@gmail.com>
2021-06-23 19:36:03 +0200

    Deployment on Heroku

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const path = require("path");
 // });
 
 router.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../build/index.html"), function (err) {
+  res.sendFile(path.join(__dirname, "../../build/index.html"), function (err) {
     if (err) {
       res.status(500).send(err);
     }
